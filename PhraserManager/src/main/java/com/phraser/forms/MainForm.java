d2d01d23e88ea2af964b29b05a1677d750b6ae5a
john <john.amirov.83@gmail.com>
2025-03-27 16:35:21 -0800

    v0.0.2

--- PhraserManager/src/main/java/com/phraser/forms/MainForm.java
+++ PhraserManager/src/main/java/com/phraser/forms/MainForm.java
@@ -198,7 +198,7 @@ public class MainForm {
     }
 
     public void showAboutDialog() {
-        Alert alert = new Alert(Alert.AlertType.NONE, "Phraser Manager v0.0.2", ButtonType.OK);
+        Alert alert = new Alert(Alert.AlertType.NONE, "Phraser Manager v0.0.3", ButtonType.OK);
         alert.showAndWait();
     }
 
